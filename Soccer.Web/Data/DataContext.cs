ba296ac0fc5de55d4d9abfd662918593ec469f90
USUARIO <USUARIO@DESKTOP-KQ4THBV>
2020-04-07 15:07:17 -0500

    Add Entities to the BD

---
+++ Soccer.Web/Data/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Soccer.Web.Data.Entities;
+
+namespace Soccer.Web.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<GroupDetailEntity> GroupDetails { get; set; }
+
+        public DbSet<GroupEntity> Groups { get; set; }
+
+        public DbSet<MatchEntity> Matches { get; set; }
+
+        public DbSet<TeamEntity> Teams { get; set; }
+
+        public DbSet<TournamentEntity> Tournaments { get; set; }
+
+    }
+}
