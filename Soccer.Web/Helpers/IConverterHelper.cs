19b09c2b19e106507584fb207ee644fe0183e747
USUARIO <USUARIO@DESKTOP-KQ4THBV>
2020-04-07 23:11:18 -0500

    Finalizar la administracion del torneo

--- Soccer.Web/Helpers/IConverterHelper.cs
+++ Soccer.Web/Helpers/IConverterHelper.cs
@@ -1,5 +1,6 @@
 ﻿using Soccer.Web.Data.Entities;
 using Soccer.Web.Models;
+using System.Threading.Tasks;
 
 namespace Soccer.Web.Helpers
 {
@@ -8,5 +9,23 @@ namespace Soccer.Web.Helpers
         TeamEntity ToTeamEntity(TeamViewModel model, string path, bool isNew);
 
         TeamViewModel ToTeamViewModel(TeamEntity teamEntity);
+
+        TournamentEntity ToTournamentEntity(TournamentViewModel model, string path, bool isNew);
+
+        TournamentViewModel ToTournamentViewModel(TournamentEntity tournamentEntity);
+
+        Task<GroupEntity> ToGroupEntityAsync(GroupViewModel model, bool isNew);
+
+        GroupViewModel ToGroupViewModel(GroupEntity groupEntity);
+
+        Task<GroupDetailEntity> ToGroupDetailEntityAsync(GroupDetailViewModel model, bool isNew);
+
+        GroupDetailViewModel ToGroupDetailViewModel(GroupDetailEntity groupDetailEntity);
+
+        Task<MatchEntity> ToMatchEntityAsync(MatchViewModel model, bool isNew);
+
+        MatchViewModel ToMatchViewModel(MatchEntity matchEntity);
+
+
     }
 }
